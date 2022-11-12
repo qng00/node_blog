8941dd5528b00cb0ff0192fc60998ad02a088746
QNG00 <89657876+qng00@users.noreply.github.com>
2022-11-12 13:33:06 +0700

    Create routes, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars').engine;
-
 const app = express();
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 // HTTP Logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', handlebars({
@@ -18,13 +24,7 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// routes init
+route(app);
 
 app.listen(port , () => console.log(`Example app listening at http://localhost:${port}`));
