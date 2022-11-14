f22ac0b4cb408ccb8401933c81284db0865d0def
QNG00 <89657876+qng00@users.noreply.github.com>
2022-11-14 13:59:28 +0700

    Instal Prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -3,28 +3,35 @@ const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars').engine;
 const app = express();
-const port = 3000
+const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 // HTTP Logger
 // app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', handlebars({
-  extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // routes init
 route(app);
 
-app.listen(port , () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`),
+);
