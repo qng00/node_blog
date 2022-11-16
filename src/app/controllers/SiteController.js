e86f3b5f52822d0332d56876e29a69ab6aec25bb
QNG00 <89657876+qng00@users.noreply.github.com>
2022-11-16 14:47:17 +0700

    CRUD - read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,17 +1,17 @@
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose')
 
 class SiteController {
     // [GET] /
-    index(req, res) {
+    index(req, res, next) {
 
-        Course.find({}, function(err, courses) {
-            if (!err) {
-                res.json(courses);
-            } else {
-                res.status(400).json({ error: 'ERROR' });
-            }
-        });
-        // res.render('home');
+        Course.find({})
+            .then(courses => {
+                res.render('home', { 
+                    courses: multipleMongooseToObject(courses)
+                });
+            })
+            .catch(next);
     }
 
     // [GET] /search
